21fb6a7aa20e2858a7b69e89b744d03142ffbc52
veriest <kankawin@hot-now.com>
2020-01-29 15:05:10 +0700

    Add str def

---
+++ bike/myapp/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Bike, Customer, Rent
+# Register your models here.
+admin.site.register(Bike)
+admin.site.register(Customer)
+admin.site.register(Rent)
+# Register your models here.
